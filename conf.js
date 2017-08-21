5f4b2ed4658065f6c77200d27d00897fa40996de
Dvorkin <pdvorkin@losx-343424-dm-n.companynet.org>
2017-08-21 16:11:53 -0400

    Commit v3

--- conf.js
+++ conf.js
@@ -1,16 +1,13 @@
+exports.config = {
+    seleniumAddress: 'http://selenium.docker:4444/wd/hub',
+    specs: ['todo-spec.js']
+};
 if (process.env.TRAVIS) {
-    exports.config = {specs: ['todo-spec.js']};
     config.sauceUser = process.env.SAUCE_USERNAME;
     config.sauceKey = process.env.SAUCE_ACCESS_KEY;
     config.capabilities = {
         'browserName': 'chrome',
         'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
         'build': process.env.TRAVIS_BUILD_NUMBER
-    };
-}
-else {
-    exports.config = {
-        seleniumAddress: 'http://selenium.docker:4444/wd/hub',
-        specs: ['todo-spec.js']
     };
 }
