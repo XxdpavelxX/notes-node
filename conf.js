bf4a30d001e2bde95e06b6be472344022be52c6e
Dvorkin <pdvorkin@losx-343424-dm-n.companynet.org>
2017-08-22 12:28:39 -0400

    bug fix

--- conf.js
+++ conf.js
@@ -2,5 +2,3 @@ exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub' || 'http://selenium.docker:4444/wd/hub',
     specs: ['todo-spec.js']
 };
-
-console.log("selenium address: ",seleniumAddress);
