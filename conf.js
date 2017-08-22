529799a07ce1e870a684904e2478e1086e8eef5b
Dvorkin <pdvorkin@losx-343424-dm-n.companynet.org>
2017-08-22 11:40:55 -0400

    selenium docker conf change

--- conf.js
+++ conf.js
@@ -1,19 +1,4 @@
-var username = "dredbound";
-var accessKey = "f4536fc1-a1f5-4b55-9519-23facc376d53";
-
 exports.config = {
-    // seleniumAddress: 'http://selenium.docker:4444/wd/hub',
-    seleniumAddress: "http://" + username + ":"+ accessKey+"@ondemand.saucelabs.com:80/wd/hub",
+    seleniumAddress: 'http://selenium.docker:4444/wd/hub',
     specs: ['todo-spec.js']
 };
-console.log("I'm outside!!!");
-if (process.env.TRAVIS) {
-    console.log("I'm inside!!!");
-    exports.config.sauceUser = process.env.SAUCE_USERNAME;
-    exports.config.sauceKey = process.env.SAUCE_ACCESS_KEY;
-    exports.config.capabilities = {
-        'browserName': 'chrome',
-        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-        'build': process.env.TRAVIS_BUILD_NUMBER
-    };
-}
