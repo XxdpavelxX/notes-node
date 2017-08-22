bec4723f9bc1267a8c878084c0482949cfbb1aae
Dvorkin <pdvorkin@losx-343424-dm-n.companynet.org>
2017-08-22 12:17:15 -0400

    Running protractor

--- conf.js
+++ conf.js
@@ -1,4 +1,5 @@
 exports.config = {
-    seleniumAddress: 'http://selenium.docker:4444/wd/hub',
+    seleniumAddress: process.env.SELENIUM_ADDRESS || 'http://selenium.docker:4444/wd/hub',
     specs: ['todo-spec.js']
 };
+console.log("Selenium Address: ", process.env.SELENIUM_ADDRESS);
